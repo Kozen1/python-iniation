0c2a5a62d1ef4f3934f95db845ac00c701595bc2
GENISSEL Benjamin <geniben@ndlp.fr>
2022-01-26 15:43:43 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+x = int(input("Choisir une colonne : "))
+y = int(input("Choisir une ligne : "))
+
+if x==4 and y==4:
+    a= print("Touché")
+elif x==5 or x==4 or x==3 or x==2 or x==1 and y==4:
+    if a== TRUE:
+        print("Coulé")
+elif x==4 or y==4:
+    print("En vue")
+else :
+    print("A l'eau")
+    
